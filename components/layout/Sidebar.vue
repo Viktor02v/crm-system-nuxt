7b71b6b927a5b61b8474a7d47483aa587d17e105
Viktor Petruk <damnnn261@gmail.com>
2024-11-06 07:53:18 +0200

    add shadcn-nuxt

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -3,9 +3,10 @@
 </script>
 
 <template>
-	<aside>
-		<NuxtLink to="/">
-			<NuxtImg width="100px" alt="" src="/favicon.ico" />
+	<aside class="px-5 py-8 bg-purple-900 h-full relative w-full">
+		<!-- TODO: Add shadecn -->
+		<NuxtLink to="/" class="mb-10 block">
+			<NuxtImg width="100px" alt="" src="/favicon.ico" class="mx-auto" />
 		</NuxtLink>
 	</aside>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import Sidebar from '~/components/layout/Sidebar.vue';
-
 
 </script>
 
