deacadd442151cb2cf6ee984ae1e7c836c4bbf2d
Viktor Petruk <damnnn261@gmail.com>
2024-11-06 08:14:49 +0200

    set up shadcn in project , load Button component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss']
+compatibilityDate: '2024-04-03',
+devtools: { enabled: true },
+modules: [
+ '@nuxt/image',
+ '@nuxtjs/tailwindcss',
+ 'shadcn-nuxt',
+ '@nuxtjs/color-mode'
+],
+shadcn: {
+				prefix:'',
+				componentDir:'./components/ui'
+}
 })
