5334f70d6f6bd3719720f15c8b3642a999c80340
Viktor Petruk <damnnn261@gmail.com>
2024-11-10 09:19:51 +0200

    create pages to eliminate errors

--- components/board/useBoardQuery.ts
+++ components/board/useBoardQuery.ts
@@ -1,4 +1,15 @@
-export function useBorderQuery() {
+import { useQuery } from "@tanstack/vue-query";
+import { BOARD_DATA } from "./board.data";
+import type { IDeal } from "~/types/deals.types";
+
+export function useBoardQuery() {
 	
-	return useQuery
+	return useQuery({
+		queryKey:['deals'],
+		queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
+		select(data){
+			const newBoard = [...BOARD_DATA]
+			const deals = data.documents as unknown as IDeal[]
+		},
+	})
 }
