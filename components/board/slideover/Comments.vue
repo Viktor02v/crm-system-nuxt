1f814265133131c5d47ac49a7e0f8e26829e215a
Viktor Petruk <damnnn261@gmail.com>
2024-11-13 20:23:26 +0200

    make a slideover => Comments.vue component to add and display the comments

--- components/board/slideover/Comments.vue
+++ components/board/slideover/Comments.vue
@@ -13,9 +13,21 @@ const card = data as unknown as IDeal
 <template>
 	<div>
 		<UiInput placeholder="Leave your comment" v-model="comment" @keyup.enter="writeComment" />
-	</div>
 
-	
+		<UiSkeleton v-if="isLoading" class="w-full h-[76px] rounded mt-5" />
+
+		<div v-else-if="card">
+			<div v-for="comment in card?.comments" :key="comment.$id" class="flex items-start mt-5">
+				<Icon name="radix-icons:chat-bubble" class="mr-3 mt-1" size="25" />
+				<div class="border-border bg-black/20 rounded p-3 w-full">
+					<div class="mb-3 text-sm">
+						Comment {{ dayjs(comment.$createdAt).format('HH:mm') }}
+					</div>
+					<p>{{ comment.text }}</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 
